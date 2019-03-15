4caf672620f0561fd1d0d303a120bae52a44604c
Christopher Leidigh <cleidigh@Gmail.com>
2019-03-15 17:33:36 -0400

    Formatting

--- src/content/options.js
+++ src/content/options.js
@@ -1,9 +1,10 @@
+/* global strftime */
 
 function granualitySwitch() {
 	var yearradio = document.getElementById('granuality1');
 	var monthradio = document.getElementById('granuality2');
-	var yearField = document.getElementById('year')
-	var monthField = document.getElementById('month')
+	var yearField = document.getElementById('year');
+	var monthField = document.getElementById('month');
 
 	if (monthradio.selected) monthField.removeAttribute("disabled");
 	else monthField.setAttribute("disabled", "true");
@@ -12,19 +13,19 @@ function granualitySwitch() {
 }
 
 function onDialogAccept() {
-	var yearField = document.getElementById('year').value
-	var monthField = document.getElementById('month').value
+	var yearField = document.getElementById('year').value;
+	var monthField = document.getElementById('month').value;
 	const mFolder = strftime(monthField);
 	const yFolder = strftime(yearField);
 
 	let illegalChars = /[^a-z0-9_()-\s]/gi;
 
-	if(illegalChars.test(mFolder)) {
+	if (illegalChars.test(mFolder)) {
 		alert('Illegal Month Foldername: ' + mFolder);
 		return false;
 	}
 
-	if(illegalChars.test(yFolder)) {
+	if (illegalChars.test(yFolder)) {
 		alert('Illegal Year Foldername: ' + yFolder);
 		return false;
 	}
--- src/content/overlay.js
+++ src/content/overlay.js
@@ -5,7 +5,7 @@
  * 1.1 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  * for the specific language governing rights and limitations under the
@@ -31,68 +31,68 @@
  * and other provisions required by the GPL or the LGPL. If you do not delete
  * the provisions above, a recipient may use your version of this file under
  * the terms of any one of the MPL, the GPL or the LGPL.
- * 
+ *
  * ***** END LICENSE BLOCK ***** */
 
 var messagearchiveoptions = {
-  preferenceService: Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService).getBranch("extensions.messagearchiveoptions@eviljeff.com."),
+	preferenceService: Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefService).getBranch("extensions.messagearchiveoptions@eviljeff.com."),
 
 	// cleidigh - use getStringPref for TB 60+
-	get monthValue() {return this.preferenceService.getStringPref("monthstring");},
-	get yearValue() {return this.preferenceService.getStringPref("yearstring");},
+	get monthValue() { return this.preferenceService.getStringPref("monthstring"); },
+	get yearValue() { return this.preferenceService.getStringPref("yearstring"); },
 
 	get keyModifiers() {
-	var modifiers=new Array();
-	if (this.preferenceService.getBoolPref("key.shift")) modifiers.push("shift");
-	if (this.preferenceService.getBoolPref("key.alt")) modifiers.push("alt");
-	if (this.preferenceService.getBoolPref("key.control")) modifiers.push("accel");
-	return modifiers;
-  },
-  onLoad: function() {
+		var modifiers = [];
+		if (this.preferenceService.getBoolPref("key.shift")) modifiers.push("shift");
+		if (this.preferenceService.getBoolPref("key.alt")) modifiers.push("alt");
+		if (this.preferenceService.getBoolPref("key.control")) modifiers.push("accel");
+		return modifiers;
+	},
+	onLoad: function () {
 
 		// cleidigh - original approach modifies batch archive function to substitute
-		// complex date strings for folder names.  
+		// complex date strings for folder names.
 
 		// Modify year and month folder names
 		// cleidigh - use strftime from @tdoan to replace deprecated toLocaleFormat in TB 60+
 
-		let rg = /let monthFolderName =.*;$/gm
-		var origfunc=((BatchMessageMover.prototype.archiveSelectedMessages)? BatchMessageMover.prototype.archiveSelectedMessages : BatchMessageMover.prototype.archiveMessages).toSource(); //function name changed in TB3.1
+		let rg = /let monthFolderName =.*;$/gm;
+		var origfunc = ((BatchMessageMover.prototype.archiveSelectedMessages) ? BatchMessageMover.prototype.archiveSelectedMessages : BatchMessageMover.prototype.archiveMessages).toSource(); // function name changed in TB3.1
 
-		origfunc=origfunc.replace(rg, 'let monthFolderName = strftime(messagearchiveoptions.monthValue, msgDate).toString();');
+		origfunc = origfunc.replace(rg, 'let monthFolderName = strftime(messagearchiveoptions.monthValue, msgDate).toString();');
 
-		origfunc=origfunc.replace('msgDate.getFullYear().toString()','strftime(messagearchiveoptions.yearValue, msgDate)');
+		origfunc = origfunc.replace('msgDate.getFullYear().toString()', 'strftime(messagearchiveoptions.yearValue, msgDate)');
 
 		// Remove function declaration and function closure so anonymous function is not nested
 		// otherwise new function fails to work
 
-		rg = /^\s*\(\s*function\s*\(.+\)\s*\{/gm
-		origfunc=origfunc.replace(rg, '');
-		
-		rg = /\}.*\)\s*$/gm
-		origfunc=origfunc.replace(rg, '');
+		rg = /^\s*\(\s*function\s*\(.+\)\s*\{/gm;
+		origfunc = origfunc.replace(rg, '');
+
+		rg = /\}.*\)\s*$/gm;
+		origfunc = origfunc.replace(rg, '');
 
 		// Rework removing eval() and replacing with new Function()
-		BatchMessageMover.prototype.archiveMessages = new Function('aMsgHdrs',origfunc);
+		BatchMessageMover.prototype.archiveMessages = new Function('aMsgHdrs', origfunc);
 
-	this.migrateOldPrefs();
-	this.observe("","nsPref:changed","");
+		this.migrateOldPrefs();
+		this.observe("", "nsPref:changed", "");
 
-  }, 
-  observe:function(subject ,topic , data) {
-	//Application.console.log("MessageArchiveOptions:observe");
-	if (topic!="nsPref:changed") return;
-	document.getElementById("key_archive").setAttribute("modifiers",this.keyModifiers.join(" "));
-  },
-  migrateOldPrefs:function() {
-	var ps=this.preferenceService;
-    if (ps.prefHasUserValue("mail.server.default.archive_granularity")) {
-		ps.setIntPref("mail.identity.default.archive_granularity", ps.getIntPref("mail.server.default.archive_granularity"))
-		ps.clearUserPref("mail.server.default.archive_granularity");
-	}
-  }
+	},
+	observe: function (subject, topic, data) {
+		// Application.console.log("MessageArchiveOptions:observe");
+		if (topic !== "nsPref:changed") return;
+		document.getElementById("key_archive").setAttribute("modifiers", this.keyModifiers.join(" "));
+	},
+	migrateOldPrefs: function () {
+		var ps = this.preferenceService;
+		if (ps.prefHasUserValue("mail.server.default.archive_granularity")) {
+			ps.setIntPref("mail.identity.default.archive_granularity", ps.getIntPref("mail.server.default.archive_granularity"));
+			ps.clearUserPref("mail.server.default.archive_granularity");
+		}
+	},
 };
 
-// cleidigh - use nsIPrefBranch for TB 60+ 
-window.addEventListener("load", function(e) { messagearchiveoptions.onLoad(e); }, false);
-messagearchiveoptions.preferenceService.QueryInterface(Components.interfaces.nsIPrefBranch).addObserver("key", messagearchiveoptions, false);
+// cleidigh - use nsIPrefBranch for TB 60+
+window.addEventListener("load", function (e) { messagearchiveoptions.onLoad(e); }, false);
+messagearchiveoptions.preferenceService.QueryInterface(Ci.nsIPrefBranch).addObserver("key", messagearchiveoptions, false);
