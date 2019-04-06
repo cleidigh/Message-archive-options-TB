7a5d37a142468b0074246787f4d019a04391af72
Christopher Leidigh <cleidigh@Gmail.com>
2019-04-06 18:06:54 -0400

    Working-TB60-68 fundamentally

--- src/content/options.js
+++ src/content/options.js
@@ -1,4 +1,25 @@
-/* global strftime */
+/* global strftime, Preferences */
+var { Services } = ChromeUtils.import('resource://gre/modules/Services.jsm');
+const { strftime } = ChromeUtils.import("chrome://messagearchiveoptions/content/strftime.js");
+
+var currentPrefs = [];
+
+// var returnValues = window.arguments[0];
+
+console.log("options load");
+Preferences.addAll([
+	{ id: "extensions.messagearchiveoptions@eviljeff.com.monthstring", type: "unichar" },
+	{ id: "extensions.messagearchiveoptions@eviljeff.com.yearstring", type: "unichar" },
+	{ id: "mail.identity.default.archive_granularity", type: "int" },
+	{ id: "extensions.messagearchiveoptions@eviljeff.com.key.alt", type: "bool" },
+	{ id: "extensions.messagearchiveoptions@eviljeff.com.key.shift", type: "bool" },
+	{ id: "extensions.messagearchiveoptions@eviljeff.com.key.control", type: "bool" },
+]);
+
+for (let element of document.querySelectorAll('[id]')) {
+	window[element.id] = element;
+}
+
 
 function granualitySwitch() {
 	var yearradio = document.getElementById('granuality1');
@@ -6,29 +27,79 @@ function granualitySwitch() {
 	var yearField = document.getElementById('year');
 	var monthField = document.getElementById('month');
 
+
+	console.log("granularity " + yearField);
 	if (monthradio.selected) monthField.removeAttribute("disabled");
 	else monthField.setAttribute("disabled", "true");
 	if (yearradio.selected || monthradio.selected) yearField.removeAttribute("disabled");
 	else yearField.setAttribute("disabled", "true");
 }
 
-function onDialogAccept() {
+function onDialogAccept(e) {
+	console.log("accept hours");
+	console.log(Preferences.getAll());
 	var yearField = document.getElementById('year').value;
 	var monthField = document.getElementById('month').value;
-	const mFolder = strftime(monthField);
-	const yFolder = strftime(yearField);
+	const mFolder = strftime.strftime(monthField);
+	const yFolder = strftime.strftime(yearField);
 
 	let illegalChars = /[^a-z0-9_()-\s]/gi;
 
 	if (illegalChars.test(mFolder)) {
-		alert('Illegal Month Foldername: ' + mFolder);
+		Services.prompt.alert(window, 'Option Error', 'Illegal Month Foldername: ' + mFolder);
+
+		console.log("accept "+mFolder);
+		e.preventDefault();
+
 		return false;
 	}
 
 	if (illegalChars.test(yFolder)) {
-		alert('Illegal Year Foldername: ' + yFolder);
+		Services.prompt.alert('Option Error', 'Illegal Year Foldername: ' + mFolder);
+		// alert('Illegal Year Foldername: ' + yFolder);
+		console.log("accept "+yFolder);
+		// e.preventDefault();
+
 		return false;
 	}
 
 	return true;
 }
+
+function onDialogCancel() {
+	console.log("dialogue cancel Mine ");
+	console.log(Preferences.getAll());
+}
+
+function onLoad(e) {
+	currentPrefs = Preferences.getAll();
+	granualitySwitch();
+}
+
+document.addEventListener('dialogaccept', function(e) {
+	onDialogAccept(e);
+	// returnValues.cancelDialog = false;
+	return false;
+});
+// document.addEventListener('dialogcancel', onDialogCancel());
+document.addEventListener('dialogcancel', function(e) {
+	let preferenceService = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefService).getBranch("extensions.messagearchiveoptions@eviljeff.com.");
+
+	console.log("dialogue cancel ");
+	// Preferences.addAll(currentPrefs);
+	console.log(Preferences.getAll()[0].value);
+	console.log(currentPrefs[0].value);
+	var yearField = document.getElementById('year');
+	var monthField = document.getElementById('month');
+	// yearField.value = currentPrefs[1].value;
+	// monthField.value = currentPrefs[0].value;
+	// preferenceService.setStringPref("monthstring", currentPrefs[0].value);
+	// preferenceService.setStringPref("yearstring", currentPrefs[1]._value);
+	// console.log(Preferences.getAll());
+
+	// returnValues.cancelDialog = true;
+	return true;
+});
+
+// window.addEventListener("load", onLoad(), false);
+window.addEventListener("load", function (e) { onLoad(e); }, false);
--- src/content/overlay.js
+++ src/content/overlay.js
@@ -1,7 +1,7 @@
 const { strftime } = ChromeUtils.import("chrome://messagearchiveoptions/content/strftime.js");
-console.error('Overland load 8 ** ' + strftime.strftime('%Y %b'));
+console.error('Overland load 66  *** ' + strftime.strftime('%Y %b'));
 
-console.log("overlay 2");
+
 
 var messagearchiveoptions = {
 	preferenceService: Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefService).getBranch("extensions.messagearchiveoptions@eviljeff.com."),
@@ -24,30 +24,55 @@ var messagearchiveoptions = {
 
 		// Modify year and month folder names
 		// cleidigh - use strftime from @tdoan to replace deprecated toLocaleFormat in TB 60+
+		console.log("overlay 5 ");
+		// console.log("granularity "+ this.yearValue());
+		// console.log("overlay "+strftime);
+		console.error('Overland load inside ' + strftime.strftime('%Y %b'));
 
 		let rg = /let monthFolderName =.*;$/gm;
 		var origfunc = ((BatchMessageMover.prototype.archiveSelectedMessages) ? BatchMessageMover.prototype.archiveSelectedMessages : BatchMessageMover.prototype.archiveMessages).toSource(); // function name changed in TB3.1
+		const ofunc = origfunc;
+		// console.log(ofunc + '\n\n');
 
-		origfunc = origfunc.replace(rg, 'let monthFolderName = strftime(messagearchiveoptions.monthValue, msgDate).toString();');
+		// origfunc = origfunc.replace(rg, 'let monthFolderName = strftime(messagearchiveoptions.monthValue, msgDate).toString();');
 
-		origfunc = origfunc.replace('msgDate.getFullYear().toString()', 'strftime(messagearchiveoptions.yearValue, msgDate)');
+		// origfunc = origfunc.replace('msgDate.getFullYear().toString()', 'strftime(messagearchiveoptions.yearValue, msgDate)');
+
+		// console.log('before\n' + origfunc + '\n\n');
 
 		// Remove function declaration and function closure so anonymous function is not nested
 		// otherwise new function fails to work
 
-		rg = /^\s*\(\s*function\s*\(.+\)\s*\{/gm;
+		// rg = /^\s*\(\s*function\s*\(.+\)\s*\{/gm;
+		rg = /^\s*archiveMessages\s*\(.+\)\s*\{/gm;
 		origfunc = origfunc.replace(rg, '');
 
-		rg = /\}.*\)\s*$/gm;
+		// rg = /\}.*\)\s*$/gm;
+		rg = /\}.*\s*$/gm;
 		origfunc = origfunc.replace(rg, '');
+
+
+		// console.log(origfunc);
 
 		// Rework removing eval() and replacing with new Function()
-		BatchMessageMover.prototype.archiveMessages = new Function('aMsgHdrs', origfunc);
+		// BatchMessageMover.prototype.archiveMessages = new Function('aMsgHdrs', 'alert("hello "+aMsgHdrs.length)');
+		// BatchMessageMover.prototype.archiveMessages = new Function('aMsgHdrs', ofunc);
+		// BatchMessageMover.prototype.archiveMessages = new Function('aMsgHdrs', origfunc);
+		BatchMessageMover.prototype.archiveMessages = messagearchiveoptions.archiveMessagesOverride;
+
+		// origfunc = ((BatchMessageMover.prototype.archiveSelectedMessages) ? BatchMessageMover.prototype.archiveSelectedMessages : BatchMessageMover.prototype.archiveMessages).toSource(); // function name changed in TB3.1
+		// console.log(origfunc);
 
 		this.migrateOldPrefs();
 		this.observe("", "nsPref:changed", "");
-
+		console.log('end of overlay');
 	},
+
+	test: function (aMsgHdrs) {
+		console.log('test function ' + aMsgHdrs.length);
+		// messagearchiveoptions.archiveMessagesOverride(aMsgHdrs, this);
+	},
+
 	observe: function (subject, topic, data) {
 		// Application.console.log("MessageArchiveOptions:observe");
 		if (topic !== "nsPref:changed") return;
@@ -59,6 +84,94 @@ var messagearchiveoptions = {
 			ps.setIntPref("mail.identity.default.archive_granularity", ps.getIntPref("mail.server.default.archive_granularity"));
 			ps.clearUserPref("mail.server.default.archive_granularity");
 		}
+	},
+
+	archiveMessagesOverride(aMsgHdrs) {
+
+		if (!aMsgHdrs.length)
+			return;
+
+		const { strftime } = ChromeUtils.import("chrome://messagearchiveoptions/content/strftime.js");
+
+		console.log('within override ');
+
+		gFolderDisplay.hintMassMoveStarting();
+		for (let i = 0; i < aMsgHdrs.length; i++) {
+			let msgHdr = aMsgHdrs[i];
+
+			let server = msgHdr.folder.server;
+
+			// Convert date to JS date object.
+			let msgDate = new Date(msgHdr.date / 1000);
+			// let msgYear = msgDate.getFullYear().toString();
+			// let monthFolderName = msgYear + "-" + (msgDate.getMonth() + 1).toString().padStart(2, "0");
+
+
+			let msgYear = strftime.strftime(messagearchiveoptions.yearValue, msgDate);
+			let monthFolderName = strftime.strftime(messagearchiveoptions.monthValue, msgDate).toString();
+
+			let archiveFolderURI;
+			let archiveGranularity;
+			let archiveKeepFolderStructure;
+
+			let identity = getIdentityForHeader(msgHdr);
+
+			if (!identity || FeedMessageHandler.isFeedFolder(msgHdr.folder)) {
+				// If no identity, or a server (RSS) which doesn't have an identity
+				// and doesn't want the default unrelated identity value, figure
+				// this out based on the default identity prefs.
+				let enabled = Services.prefs.getBoolPref(
+					"mail.identity.default.archive_enabled"
+				);
+				if (!enabled)
+					continue;
+
+				archiveFolderURI = server.serverURI + "/Archives";
+				archiveGranularity = Services.prefs.getIntPref(
+					"mail.identity.default.archive_granularity"
+				);
+				archiveKeepFolderStructure = Services.prefs.getBoolPref(
+					"mail.identity.default.archive_keep_folder_structure"
+				);
+			} else {
+				if (!identity.archiveEnabled)
+					continue;
+
+				archiveFolderURI = identity.archiveFolder;
+				archiveGranularity = identity.archiveGranularity;
+				archiveKeepFolderStructure = identity.archiveKeepFolderStructure;
+			}
+
+			let copyBatchKey = msgHdr.folder.URI;
+			if (archiveGranularity >= Ci.nsIMsgIdentity
+				.perYearArchiveFolders)
+				copyBatchKey += "\0" + msgYear;
+
+			if (archiveGranularity >= Ci.nsIMsgIdentity
+				.perMonthArchiveFolders)
+				copyBatchKey += "\0" + monthFolderName;
+
+			if (archiveKeepFolderStructure)
+				copyBatchKey += msgHdr.folder.URI;
+
+			// Add a key to copyBatchKey
+			if (!(copyBatchKey in this._batches)) {
+				this._batches[copyBatchKey] = {
+					srcFolder: msgHdr.folder,
+					archiveFolderURI,
+					granularity: archiveGranularity,
+					keepFolderStructure: archiveKeepFolderStructure,
+					yearFolderName: msgYear,
+					monthFolderName,
+					messages: [],
+				};
+			}
+			this._batches[copyBatchKey].messages.push(msgHdr);
+		}
+		MailServices.mfn.addListener(this, MailServices.mfn.folderAdded);
+
+		// Now we launch the code iterating over all message copies, one in turn.
+		this.processNextBatch();
 	},
 };
 
